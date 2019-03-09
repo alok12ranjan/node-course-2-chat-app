03b170f6840e6ad3bad2067b71148fdf280a9f8e
alok12ranjan <alok12.ranjan@gmail.com>
2019-03-09 20:41:14 +0530

    Port Number has updated.

--- server/server.js
+++ server/server.js
@@ -9,6 +9,6 @@ const port = process.env.PORT || 3000;
 var app = express();
 app.use(express.static(publicPath));
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log(`server is up on ${port}`);
 });
